3b222ce623a683433c96bea26ed15a5b23bca0e0
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-25 21:23:27 -0400

    Muna4029__diff__id_34

--- import_export/admin.py
+++ import_export/admin.py
@@ -744,7 +744,6 @@ class ExportMixin(BaseExportMixin, ImportExportMixinBase):
 
         original_show_full_result_count = self.show_full_result_count
         self.show_full_result_count = False
-
         class FakePaginator:
             count = 0
         original_get_paginator = self.get_paginator
